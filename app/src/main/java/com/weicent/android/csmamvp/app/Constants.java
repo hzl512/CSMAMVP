bd064b137c3a2630ccf5043042ddd752b1a44433
10001049 <zonglei.huang@nflg.com>
2021-03-17 09:28:18 +0800

    【校园二手交易市场100】[update]根目录修改以及主页竖屏设置；

--- app/src/main/java/com/weicent/android/csmamvp/app/Constants.java
+++ app/src/main/java/com/weicent/android/csmamvp/app/Constants.java
@@ -7,12 +7,14 @@ package com.weicent.android.csmamvp.app;
 public class Constants {
 
     //基础URL
-    public static final String BASE_URL = "http://192.168.31.169:8080/CampusSecondaryMarket/";
-    //本地URL
-//    public static final String BASE_URL = "http://10.0.2.2:8080/CampusSecondaryMarket/";
+    public static final String BASE_URL = "http://pc-huangzonglei.nflg:8090/CampusSecondaryMarket/";
+//    public static final String BASE_URL = "http://192.168.31.169:8080/CampusSecondaryMarket/";
+//    本地URL
+//    public static final String BASE_URL = "http://10.0.2.2:8090/CampusSecondaryMarket/";
+
 
     //网络连接超时时间
-    public static final int NET_OUT_TIME=10000;//10秒
+    public static final int NET_OUT_TIME = 10000;//10秒
 
     //网络提示
     public static final String NET_OUT_MSG = "请求失败或超时，请稍后再试";
@@ -27,33 +29,33 @@ public class Constants {
     public static final String INTENT_KEY_ID = "id";
 
     //商品添加URL
-    public static final String URL_COMMODITY_ADD_SERVLET="CommodityAddServlet";
+    public static final String URL_COMMODITY_ADD_SERVLET = "CommodityAddServlet";
     //商品URL
-    public static final String URL_COMMODITY_SERVLET="CommodityServlet";
+    public static final String URL_COMMODITY_SERVLET = "CommodityServlet";
     //用户URL
-    public static final String URL_USERS_SERVLET="UsersServlet";
+    public static final String URL_USERS_SERVLET = "UsersServlet";
     //求购URL
-    public static final String URL_BUYS_SERVLET="BuysServlet";
+    public static final String URL_BUYS_SERVLET = "BuysServlet";
     //分类URL
-    public static final String URL_CATEGORY_SERVLET="CategoryServlet";
+    public static final String URL_CATEGORY_SERVLET = "CategoryServlet";
     //院系URL
-    public static final String URL_DEPARTMENTS_SERVLET="DepartmentsServlet";
+    public static final String URL_DEPARTMENTS_SERVLET = "DepartmentsServlet";
     //专业URL
-    public static final String URL_PROFESSION_SERVLET="ProfessionServlet";
+    public static final String URL_PROFESSION_SERVLET = "ProfessionServlet";
     //更新URL
-    public static final String URL_UPDATE_SERVLET="UpdateServlet";
+    public static final String URL_UPDATE_SERVLET = "UpdateServlet";
 
     //下载更新
     public final static String DOWNLOAD_FILE_APK_NAME = "csma.apk";
-    public final static String DOWNLOAD_UPDATE_SERVICE_BAG_NAME="com.weicent.android.csmamvp.services.DownloadUpdateService";
+    public final static String DOWNLOAD_UPDATE_SERVICE_BAG_NAME = "com.weicent.android.csmamvp.services.DownloadUpdateService";
     public final static String
-            UPDATE_APP_NAME="appname",  //版本名称
-            UPDATE_VERSION="version",    // 版本号
-            UPDATE_FORCED_UPDATE="forcedupdate", //是否强制更新
-            UPDATE_DESCRIPTION="description",//描述
-            UPDATE_URL="update_url",  //新app下载地址
-            UPDATE_TIME="update_time", //更新时间
-            UPDATE_SIZE="update_size", //apk大小描述
-            UPDATE_APKSIZE="update_apksize";//apk大小
+            UPDATE_APP_NAME = "appname",  //版本名称
+            UPDATE_VERSION = "version",    // 版本号
+            UPDATE_FORCED_UPDATE = "forcedupdate", //是否强制更新
+            UPDATE_DESCRIPTION = "description",//描述
+            UPDATE_URL = "update_url",  //新app下载地址
+            UPDATE_TIME = "update_time", //更新时间
+            UPDATE_SIZE = "update_size", //apk大小描述
+            UPDATE_APKSIZE = "update_apksize";//apk大小
 
 }
